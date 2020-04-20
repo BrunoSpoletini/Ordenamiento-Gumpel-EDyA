0996e75382afe5bc45b212e79b384a70e9283027
Pomarola <lpoma2000@gmail.com>
2020-04-20 17:38:33 -0300

    Se rompe a veces

--- main.c
+++ main.c
@@ -13,8 +13,7 @@ char * lugarDeNacimiento ; // pais o capital
 
 // Dado el nombre de un archivo, devuelve la cantidad de lineas escritas.
 // contar_lineas: char[] -> int.
-int contar_lineas(char nombre_archivo[]){
-    FILE* fp = fopen(nombre_archivo, "r");
+int contar_lineas(FILE* fp){
     int numero_lineas = 0;
     if(fp != NULL) { // Si se puede abrir el archivo.
         char buffer[1010]; // Buffers
@@ -27,7 +26,7 @@ int contar_lineas(char nombre_archivo[]){
     {
         printf("No se pudo abrir el archivo que se quizo contar sus lineas.\n");
     }
-    fclose(fp);
+    rewind(fp);
     return numero_lineas;
 }
 
@@ -38,17 +37,20 @@ int greater_equal(const void *a, const void *b){
    return ( *(int*)a - *(int*)b );
 }
 
-int *genera_lista_random(int numLineas,int numDatos){
+int *genera_lista_random(FILE *fp,int numDatos){
     int i, random;
+
+    int numLineas = contar_lineas(fp);
+
     int *arrayContinuo = malloc(sizeof(int) * numLineas);
     int *arrayRandoms = malloc(sizeof(int) * numDatos);
-    for(i=0; i<numLineas; i++){
+    for(i=0;i<numLineas;i++){
         arrayContinuo[i]=i;
     }
     for(i = 0; i < numDatos; i++){
-        random = rand() % (numLineas-i+1);
-        arrayRandoms[i]=arrayContinuo[random];
-        arrayContinuo[random]=arrayContinuo[numLineas-i];
+    random = rand() % (numLineas-i);
+    arrayRandoms[i]=arrayContinuo[random];
+    arrayContinuo[random]=arrayContinuo[numLineas-i];
     }
     
     free (arrayContinuo);
@@ -60,16 +62,17 @@ int *genera_lista_random(int numLineas,int numDatos){
 // leerArchivos toma un puntero a un archivo, una lista de enteros (las líneas a leer del archivo), 
 // la cantidad de líneas a leer y la cantidad de líneas que hay en el archivo a leer.
 // Devuelve una lista de cadenas cuyos elementos son las líneas del archivo leído.
-Persona *leer_archivo_personas(char nombreArchivo[], int *arrayRandoms, int numDatos){
+Persona *leer_archivo_personas(char nombreArchivo[], int numDatos){
     FILE* fp = fopen(nombreArchivo, "r");
-
     int cantLeidos=0, i;
     char buffer[1010];
+    
+    int *arrayRandoms = genera_lista_random(fp, numDatos);
+
     Persona *listaPersonas = malloc(sizeof(Persona)*numDatos);
+
     for(i=0; cantLeidos < numDatos; i++){
         fscanf(fp,"%[^\n]\n", buffer);
-        //fgets(buffer,1010,fp);
-        //printf("%d\n",i);
         if(i == arrayRandoms[cantLeidos]){
             listaPersonas[cantLeidos].edad = (rand() % 100) + 1;
             listaPersonas[cantLeidos].nombre = malloc(sizeof(buffer));
@@ -77,17 +80,21 @@ Persona *leer_archivo_personas(char nombreArchivo[], int *arrayRandoms, int numD
             cantLeidos++;
         }
     }
+    fclose(fp);
+    free(arrayRandoms);
     return listaPersonas;
 }
 /*/
 void leer_archivo_paises(char *nombre, int numDatos, Persona *listaPersonas){
     int i;
     FILE* fp = fopen(nombre, "r");
+    int numLineas = contar_lineas(fp);
+
     for(i = 0 ; (fscanf(fp, "%[^\n]\n", linea) != EOF); ++i){ // Mientras la linea no este vacia
-        strcpy(matriz_personas[i],linea); // Coloca la linea en la matriz
+        strcpy(arrayPaises[i],linea); // Coloca la linea en la matriz
         }
     
-}//*/
+}/*/
 
 void liberar_personas(Persona *listaPersonas, int numDatos){
     int i;
@@ -101,32 +108,24 @@ void liberar_personas(Persona *listaPersonas, int numDatos){
 int main(){
     srand (time(NULL));
     int i=0; //Debbug
-    int numDatos, numLineas, *arrayRandoms;
+    int numDatos, numLineas;
     printf("Ingrese el numero de datos a generar:\n");
     scanf("%d", &numDatos);
     Persona *listaPersonas;
 
 
-    numLineas = contar_lineas("nombres1.txt");
+    
 
-    arrayRandoms = genera_lista_random(numLineas, numDatos);
-
-
-    listaPersonas = leer_archivo_personas("nombres1.txt", arrayRandoms, numDatos);
-
-
+    listaPersonas = leer_archivo_personas("nombres3.txt", numDatos);
 
     //listaPersonas = leer_archivo_paises("paises.txt", numDatos, listaPersonas);
-
-    
 
     //debbug
     for(i=0;i<numDatos;i++){
     printf("%s\n",listaPersonas[i].nombre);
-    //printf("%d\n",arrayRandoms[i]);
     }
-    free(arrayRandoms);
-    //liberar_personas(listaPersonas, numDatos);
+    
+    liberar_personas(listaPersonas, numDatos);
     
     return 0;
 }
